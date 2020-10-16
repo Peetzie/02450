15b93912813e000e198a3ef2a356e4c53d773f0c
Peetzie <s174159@student.dtu.dk>
2020-10-16 17:13:33 +0200

    Added Homwork script

---
+++ ReadingAssignments.py
@@ -0,0 +1,6 @@
+## Reading assignment answers and stuff ## 
+
+# Homework Problem 2.1
+# The answer is B. Both age and PV there a natual zero and we can apply operatis. Thus they are ratio. 
+# Race, HT and UI are binary, categorial. 
+# Mw is contiunuous and PV is discrete. 
