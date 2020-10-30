18f4984739b0188024487e18cbdf61c4dfd5ef97
Peetzie <s174159@student.dtu.dk>
2020-10-30 16:19:53 +0100

    Modded to show cos graph

--- 02450Toolbox_Python/Scripts/ex0_5_1.py
+++ 02450Toolbox_Python/Scripts/ex0_5_1.py
@@ -1,12 +1,17 @@
 ## exercise 0.5.1
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-x = np.arange(0, 1, 0.1)
+# Original
+x = np.arange(0, math.pi, 0.1)
 f = np.exp(x)
+#Modded
+f_mod = np.cos(x)
+
 
 plt.figure(1)
-plt.plot(x, f)
+plt.plot(x, f_mod)
 plt.xlabel('x')
 plt.ylabel('f(x)=exp(x)')
 plt.title('The exponential function')
